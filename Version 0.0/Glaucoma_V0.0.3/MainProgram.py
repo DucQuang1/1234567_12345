20c0669f03aeea1df1fbd5b9bb675c8d65751caa
DucQuang1 <nguyen.ducquang@vjc.sg>
2015-06-05 09:11:10 +0800

    Adding new graphs and new files

--- Version 0.0/Glaucoma_V0.0.3/MainProgram.py
+++ Version 0.0/Glaucoma_V0.0.3/MainProgram.py
@@ -20,7 +20,7 @@ from PlottingSystemClass import PlottingSystem
 from SimulationSystemClass import SimulationSystem
 plottingsystem = PlottingSystem(plt)
 order = 1
-for i in range(1):
+for i in range(30):
     sysSimulation = SimulationSystem(3000,"PatientList/Patients_list_{}.csv".format(i))
     sysSimulation.SystemSimulation()
     plottingsystem.plot(sysSimulation,order,i,masterListforReplications)
